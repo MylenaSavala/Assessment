fe534ec3370c3667fc22398236af6be0e9db17fd
Mylena Savala <79485071+MylenaSavala@users.noreply.github.com>
2022-03-25 22:41:37 -0300

    versaoEntrega

--- src/pages/home/cena.js
+++ src/pages/home/cena.js
@@ -1,35 +1,16 @@
-import { useContext,useEffect,useState} from "react";
 import './styles.scss';
-import GlobalStateContext from "../../context/GlobalStateContext";
-import {goToQuiz} from "../../Router/coordinator";
 import Profile from "../../components/profile";
 import Quiz from "../../pages/quiz";
 
-import { useNavigate } from "react-router-dom";
 
 const Cena = () =>{ 
 
-  const {data} = useContext(GlobalStateContext);
-  const dataNascimento=data.dataNascimento;
-  const identificacao=data.identificacao;
-  const navigation = useNavigate();
-
-  useEffect(() => {
-  }, []);
-
-
   return (
-<div className="body">
-<div className="Container">
-
-<Profile />   
+<div className='bodyCena'>
+<Profile />  
 <Quiz />
-    {/* <fieldset>  
-      <input type="button" className="button" id="btnSubmit" value="Login" onClick={() =>goToQuiz(navigation)}/>
-    </fieldset> */}
-    </div>
+</div>
 
-    </div>
   );
 }
 
--- src/pages/result/index.js
+++ src/pages/result/index.js
@@ -1,61 +1,43 @@
-import { useState,useContext } from "react";
+import {useContext,useState,useEffect } from "react";
 import GlobalStateContext from "../../context/GlobalStateContext";
+import Profile from "../../components/profile";
+
 
 function Result() {
+  const [profileDEV, setProfile] = useState('');
+  const [age, setAge] = useState(0);
 
-  // const [score, setScore] = useState(0);
-  // const [showScore, setShowScore] = useState(false);
-  // const [currentQuestion, setCurrentQuestion] = useState(0);
+  const {data} = useContext(GlobalStateContext); 
+  useEffect(() => {
+    resultFinish()
+    searchAge()
+  }, []);
 
 
-  // function handleAnswer(isCorrect) {
-  //   if (isCorrect) {
-  //     setScore(score + 10);
-  //   }else if (isCorrect===false){
-  //     setScore(score - 10);
-  //   }else{
-  //     setScore(score);
-  //   }
+  const searchAge = () =>{
+    var currentTime = new Date();
+    var year = currentTime.getFullYear()
+    var age = Number(data.dataNascimento.getFullYear) -year;
+    setAge(age)
+  }
+const resultFinish =()=>{
+ if(data.result>=-50&&data.result>0){
+    console.log("Você tem o perfil de FrontEnd!")
+setProfile("Você tem o perfil de FrontEnd!")
+  }else if(data.result===-50+50){
+    setProfile("Indefinido! Refaça o teste novamente!")
+  }else{
+    setProfile("Você tem o perfil de BackEnd!")
+  }
+} 
 
-  //   const nextQuestion = currentQuestion + 1;
-  //   if (nextQuestion < questions.length) {
-  //     setCurrentQuestion(nextQuestion);
-  //   } else {
-  //     setShowScore(true);
-  //   }
-  // }
-
-return (
+  return (
+    <div className="result">
   <div className="app">
-  {/* {showScore ? (
-    <div className="score-section">
-      Você pontuou {score} de {questions.length}
-    </div>
-  ) : (
-    <>
-      <div className="question-section">
-        <div className="question-count">
-          <span>Questão {currentQuestion + 1}</span>/{questions.length}
-        </div>
-        <div className="question-text">
-          {questions[currentQuestion].questionText}
-        </div>
-      </div>
+  <Profile />
 
-      <div className="answer-section">
-        {questions[currentQuestion].answerOptions.map(
-          (answerOption, index) => (
-            <button
-              onClick={() => handleAnswer(answerOption.isCorrect)}
-              key={index}
-            >
-              {answerOption.answerText}
-            </button>
-          )
-        )}
-      </div>
-    </>
-  )} */}
+  <h1>{profileDEV}</h1>
+</div>
 </div>
     )
 }
